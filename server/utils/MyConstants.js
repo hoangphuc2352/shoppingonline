c9d164133efdea95d556f5b195dea9af27a24075
hoangphuc2352 <165146887+hoangphuc2352@users.noreply.github.com>
2024-07-16 13:22:29 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,13 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.dwx18k7.mongodb.net',
+  DB_USER: 'phuc1',
+  DB_PASS: 'phuc1',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'hoangphuc22',
+  JWT_EXPIRES: '31556952000', // in milliseconds (01 year = 31556952000 ms)
+  EMAIL_USER: 'ngotdung2002@hotmail.com', // gmail service --Microsoft mail service
+  EMAIL_PASS: 'ngotiendung123'
+};
+module.exports = MyConstants;
+
+//mongodb+srv://tiendung8a6:C5QavQodd71CrZ0c@onlineshopping.7eavwgm.mongodb.net/?retryWrites=true&w=majority
